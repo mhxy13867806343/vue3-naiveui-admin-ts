17e6e4bc480aa0c72730b3bd4ac751bf9f12dade
(2**16)-1=65535 <869710179qq.com>
2026-04-30 10:33:06 +0800

    fix: change router history to hash to solve Github Pages 404 issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
  * 路由实例与静态路由配置
  * 静态路由包含 /login、/register、/401、/403、/404、/500
  */
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 
 export const staticRoutes: RouteRecordRaw[] = [
   {
@@ -665,7 +665,7 @@ export const staticRoutes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: staticRoutes,
 })
 
